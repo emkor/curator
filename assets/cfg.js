32dc2624b3edcf63db9ba43e6b244bd1c0ad7dc1
emkor93 <emkor93@gmail.com>
2019-05-04 16:17:26 +0200

    updated callback url, added client id placeholder

--- assets/cfg.js
+++ assets/cfg.js
@@ -1,8 +1,8 @@
 appConfig = {
     authUrl: "https://accounts.spotify.com/authorize",
     apiUrl: "https://api.spotify.com/v1",
-    callbackUrl: "http://localhost:8080/",
-    clientId: "",
+    callbackUrl: "http://localhost:8080",
+    clientId: "CLIENT_ID_PLACEHOLDER",
     scopes: [
         "playlist-modify-private",
         "playlist-modify-public",
