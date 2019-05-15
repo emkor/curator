cd378d332af8fb36bacbd07609ec0a72ba976744
emkor93 <emkor93@gmail.com>
2019-05-15 21:35:53 +0200

    reduced scripts to single one

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-mkdir -p dep
+set -e
 
+echo "Cleanup..."
+rm -rf dep
+rm -rf dist
+
+echo "Copying dependencies..."
+mkdir -p dep
 cp node_modules/bootswatch/dist/darkly/bootstrap.min.css dep
 cp -R node_modules/font-awesome/css dep
 cp node_modules/tablesort/tablesort.css dep
-
 cp node_modules/jquery/dist/jquery.min.js dep
 cp node_modules/popper.js/dist/umd/popper.min.js dep
 cp node_modules/bootstrap/dist/js/bootstrap.min.js dep
@@ -14,5 +19,10 @@ cp node_modules/tablesort/dist/tablesort.min.js dep
 cp node_modules/tablesort/dist/sorts/tablesort.number.min.js dep
 cp -R node_modules/font-awesome/fonts dep
 
-cp index.html dist
+echo "Building distribution..."
+mkdir -p dist
+cp -R dep dist
 cp -R src dist
+cp index.html dist
+
+echo "Done"
--- dist.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir dist
-
-cp -R dep dist
-cp -R src dist
-cp index.html dist
