b8222df50164c56ac563e97b854e064950ebf57f
emkor93 <emkor93@gmail.com>
2019-05-22 14:31:02 +0200

    added dummy cfg.js file

---
+++ src/cfg.js
@@ -0,0 +1,12 @@
+appConfig = {
+    authUrl: "https://accounts.spotify.com/authorize",
+    apiUrl: "https://api.spotify.com/v1",
+    callbackUrl: "http://localhost:8080",
+    clientId: "CLIENT_ID_PLACEHOLDER",
+    scopes: [
+        "playlist-modify-private",
+        "playlist-modify-public",
+        "playlist-read-private",
+        "user-library-read"
+    ]
+};
