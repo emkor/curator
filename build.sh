0473efc8814b2da0301b8b00c7b15daa39a3fdd6
emkor93 <emkor93@gmail.com>
2020-02-23 16:06:00 +0100

    add map js files as they're required by the browser(?)

--- build.sh
+++ build.sh
@@ -13,7 +13,9 @@ cp -R node_modules/font-awesome/css dep
 cp node_modules/tablesort/tablesort.css dep
 cp node_modules/jquery/dist/jquery.min.js dep
 cp node_modules/popper.js/dist/umd/popper.min.js dep
+cp node_modules/popper.js/dist/umd/popper.min.js.map dep
 cp node_modules/bootstrap/dist/js/bootstrap.min.js dep
+cp node_modules/bootstrap/dist/js/bootstrap.min.js.map dep
 cp node_modules/vue/dist/vue.min.js dep
 cp node_modules/tablesort/dist/tablesort.min.js dep
 cp node_modules/tablesort/dist/sorts/tablesort.number.min.js dep
